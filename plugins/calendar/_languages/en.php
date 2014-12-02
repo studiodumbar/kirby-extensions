40558ae4bf8ef307144dfc1ef926ce994c5a3d12
Jonathan van Wunnik <jonathan@artlantis.nl>
2014-12-02 16:48:36 +0100

    Add Calendar plugin.

---
+++ plugins/calendar/_languages/en.php
@@ -0,0 +1,19 @@
+<?php
+
+// Calendar plugin
+// For `strftime` parameters, see http://php.net/manual/en/function.strftime.php
+
+l::set('calendar-time-format', '%d');
+l::set('calendar-full-time-format', '%d %X');
+l::set('calendar-month-format', '%b %Y');
+l::set('calendar-day-month-format', '%e %b');
+l::set('calendar-day-month-year-format', '%e %b %Y');
+l::set('calendar-no-entry', 'There are currenly no upcoming events&hellip;');
+
+l::set('date', 'Date');
+l::set('to', 'to');
+
+l::set('title', 'Title');
+l::set('description', 'Description');
+
+?>
---
+++ plugins/calendar/_languages/nl.php
@@ -0,0 +1,19 @@
+<?php
+
+// Calendar plugin
+// For `strftime` parameters, see http://php.net/manual/en/function.strftime.php
+
+l::set('calendar-time-format', '%d');
+l::set('calendar-full-time-format', '%d %X');
+l::set('calendar-month-format', '%B %Y');
+l::set('calendar-day-month-format', '%e %b');
+l::set('calendar-day-month-year-format', '%e %b %Y');
+l::set('calendar-no-entry', 'Er zijn momenteel geen aanstaande evenementen&hellip;');
+
+l::set('date', 'Datum');
+l::set('to', 'tot');
+
+l::set('title', 'Titel');
+l::set('description', 'Beschrijving');
+
+?>
